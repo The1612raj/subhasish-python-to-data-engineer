0576f7701527d3152f85ffcc6aa4a4e441d28c5c
Subhasish Panda <104180552+The1612raj@users.noreply.github.com>
2025-05-09 05:14:57 +0530

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,6 @@
+import math_utils
+
+print("Addition:", math_utils.add(10, 5))
+print("Subtraction:", math_utils.subtract(10, 5))
+print("Multiplication:", math_utils.multiply(10, 5))
+print("Division:", math_utils.divide(10, 5))
